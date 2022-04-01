cb6a3a3820c3f83ba4c50878d64f91e3c1146d7c
Deepakchavan27 <deepakch455@gmail.com>
2022-04-02 00:23:57 +0530

    correction

--- signin.js
+++ signin.js
@@ -1,22 +1,20 @@
 document.querySelector("form").addEventListener("submit", signin);
 var regdUsers = JSON.parse(localStorage.getItem("userdata"));
-console.log(regdUsers);
+// console.log(regdUsers);
 
 function signin() {
   event.preventDefault();
   var enteredEmail = document.querySelector("#inputemail").value;
   //console.log(email);
 
-  for (var i = 0; i < regdUsers.length; i++) {
-    console.log(regdUsers[i]);
-    if (
-      regdUsers[i].email == enteredEmail
-    ) {
+  for (var i = 0; i < regdUsers.length; i++){
+    //console.log(regdUsers[i]);
+    if (regdUsers[i].email == enteredEmail){
         //console.log( regdUsers[i].email);
       alert("login success");
       window.location.href = "index.html";
       break;
-    } else if(regdUsers[i].email != enteredEmail){
+    } else{
       alert("login failed");
     }
   }
