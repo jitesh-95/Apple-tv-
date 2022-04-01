2af103436a775b0fd8cd9e800a9dedbfdc7f420f
Deepakchavan27 <deepakch455@gmail.com>
2022-04-01 23:25:02 +0530

    sign in page correction

--- signin.js
+++ signin.js
@@ -1,22 +1,24 @@
 document.querySelector("form").addEventListener("submit", signin);
 var regdUsers = JSON.parse(localStorage.getItem("userdata"));
-//console.log(regdUsers);
+console.log(regdUsers);
 
 function signin() {
   event.preventDefault();
   var enteredEmail = document.querySelector("#inputemail").value;
-  console.log(email);
+  //console.log(email);
 
   for (var i = 0; i < regdUsers.length; i++) {
     console.log(regdUsers[i]);
     if (
       regdUsers[i].email == enteredEmail
     ) {
+        //console.log( regdUsers[i].email);
       alert("login success");
-      window.location.href = "mens.html";
+      window.location.href = "index.html";
       break;
-    } else {
-      console.log("login failed");
+    } else if(regdUsers[i].email != enteredEmail){
+      alert("login failed");
+      
     }
   }
 }
