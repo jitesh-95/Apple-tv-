fcb01eb1e874ee64c5bff90f43a93f0cad4889bb
Deepakchavan27 <deepakch455@gmail.com>
2022-04-01 21:57:32 +0530

    correction in signun page

--- signin.js
+++ signin.js
@@ -1,23 +1,22 @@
-document.querySelector("form").addEventListener("submit",signin);
+document.querySelector("form").addEventListener("submit", signin);
+var regdUsers = JSON.parse(localStorage.getItem("userdata"));
+//console.log(regdUsers);
 
-var usersignin = JSON.parse(localStorage.getItem("userdata"));
-console.log(usersignin);
+function signin() {
+  event.preventDefault();
+  var enteredEmail = document.querySelector("#inputemail").value;
+  console.log(email);
 
-function signin(){
-    event.preventDefault();
-
-    var appleId = document.querySelector("#inputemail").value;
-
-    console.log(appleId);
-    for(var i = 0; i<usersignin; i++){
-
-        if(usersignin[i] == appleId){
-            alert("Login Successfull");
-            window.location.href = "index.html";
-            break;
-        }
-        else{
-            alert("Login failed");
-        }
+  for (var i = 0; i < regdUsers.length; i++) {
+    console.log(regdUsers[i]);
+    if (
+      regdUsers[i].email == enteredEmail
+    ) {
+      alert("login success");
+      window.location.href = "mens.html";
+      break;
+    } else {
+      console.log("login failed");
     }
+  }
 }
