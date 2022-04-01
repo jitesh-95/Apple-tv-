c1ba3efc505d06535ca0bcb505096223238ad514
Deepakchavan27 <deepakch455@gmail.com>
2022-04-02 00:41:26 +0530

    corect signin

--- signin.js
+++ signin.js
@@ -1,21 +1,24 @@
-document.querySelector("form").addEventListener("submit", signin);
+document.querySelector("form").addEventListener("submit", loginFun);
 var regdUsers = JSON.parse(localStorage.getItem("userdata"));
-// console.log(regdUsers);
+//console.log(regdUsers);
 
-function signin() {
+function loginFun() {
   event.preventDefault();
   var enteredEmail = document.querySelector("#inputemail").value;
+
   //console.log(email);
 
-  for (var i = 0; i < regdUsers.length; i++){
-    //console.log(regdUsers[i]);
-    if (regdUsers[i].email == enteredEmail){
-        //console.log( regdUsers[i].email);
+  for (var i = 0; i < regdUsers.length; i++) {
+    console.log(regdUsers[i]);
+    if (
+      regdUsers[i].email == enteredEmail
+   
+    ) {
       alert("login success");
       window.location.href = "index.html";
       break;
-    } else{
-      alert("login failed");
+    } else {
+      console.log("login failed");
     }
   }
 }
